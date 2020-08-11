d1828e772cd8a8f4e31e6cc4d0a6fc9bc465f9ab
amorimevalero <69227939+amorimevalero@users.noreply.github.com>
2020-08-11 20:41:51 -0300

    atualizacao da hora

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+const URL_BACKEND_TOP = window.location.href.includes('localhost')
+  ? 'http://localhost:8080'
+  : 'https://devsoltinhoflix.herokuapp.com';
+
+  export default {
+    URL_BACKEND_TOP,
+  };
