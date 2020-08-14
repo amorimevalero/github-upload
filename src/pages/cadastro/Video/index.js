7e4e9b8ab86f79f1031e1b144e65a17df63c1c16
amorimevalero <69227939+amorimevalero@users.noreply.github.com>
2020-08-13 23:17:03 -0300

    commit da hora

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,11 +1,68 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import useForm from '../../../hooks/useForm';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
 
 function CadastroVideo() {
+  const history = useHistory();
+  const{handleChange, values} = useForm({
+    titulo: 'Video padrão',
+    url:'https://mariosouto.com/flappy-bird-devsoutinho/',
+    categoria: 'Front Emd',    
+  });
+
   return (
     <PageDefault>
       <h1>Cadastro de Video</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        // alert('Vídeo Cadastrado com sucesso !!!');
+
+        videosRepository.create({
+          titulo: values.titulo,
+          url: values.url,
+          categoriaId: 1,
+        })
+         .then(() => {
+
+           console.log('CAdastrado com sucesso !!!') 
+           history.push('/');
+         });
+      }}
+      >
+        <FormField
+          label="Título do Vídeo:"
+          name="titulo"
+          value={values.titulo}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="URL:"
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Categoria:"
+          name="categoria"
+          value={values.categoria}
+          onChange={handleChange}
+        />
+      
+        <Button type="submit">
+           Cadastrar
+        </Button>
+      
+      </form>
+
+      
+
 
       <Link to="/cadastro/categoria">
         Cadastrar Categoria
